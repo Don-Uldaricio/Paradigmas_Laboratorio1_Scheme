830b8d1447fe9d5e43ee007ce1d85600f2919772
Nicolas Aguilera <nicolas.aguilera.g@usach.cl>
2022-08-26 16:52:36 -0400

    Funciones comentadas / compressed? creada

--- funciones.rkt
+++ funciones.rkt
@@ -7,7 +7,6 @@
 
 (define (pixbit? pixel)
   (if (and
-       (= (length pixel) 4)
        (or (= (list-ref pixel 2) 0) (= (list-ref pixel 2) 1))) #t #f))
 
 ;Crear pixrgb
@@ -15,7 +14,6 @@
 
 (define (pixrgb? pixel)
   (if (and
-       (= (length pixel) 6)
        (and (>= (list-ref pixel 2) 0) (<= (list-ref pixel 2) 255))
        (and (>= (list-ref pixel 3) 0) (<= (list-ref pixel 3) 255))
        (and (>= (list-ref pixel 4) 0) (<= (list-ref pixel 4) 255)))#t #f))
@@ -25,33 +23,29 @@
 
 (define (pixhex? pixel)
   (if (and
-       (= (length pixel) 4)
        (string? (list-ref pixel 2))
        (= (string-length (list-ref pixel 2)) 7)) #t #f))
 
-;TDA IMAGE---------------------------------------------------------
+; TDA IMAGE -----------------------------------------------------
 
-;Crear image (pixmap/pixrgb/pixhex)
-(define (image width height . pixlist) (list width height pixlist))
+; CONSTRUCTORES -------------------------------------------------
 
-;Funciones booleanas (bitmap? / pixmap? / hexmap?)
-(define (bitmap? imagen)
-  (if (and (pixbit? (list-ref (list-ref imagen 2) 0))
-           (= (length (list-ref imagen 2)) (* (list-ref imagen 0) (list-ref imagen 1)))) #t #f))
+; Dominio: Width (int) X Height (int) X [pixbit-d* |  pixrgb-d* | pixhex-d*]
+; Recorrido: image
+; Descripción: Crea una una imagen de tipo pixmap/bitmap/hexmap
+;              poniéndolos en una lista
+; Recursión: No aplica
+(define (image width height . pixlist)
+  (list width height pixlist))
 
-(define (pixmap? imagen)
-  (if (and (pixrgb? (list-ref (list-ref imagen 2) 0))
-           (= (length (list-ref imagen 2)) (* (list-ref imagen 0) (list-ref imagen 1)))) #t #f))
+; MODIFICADORES -------------------------------------
 
-(define (hexmap? imagen)
-  (if (and (pixhex? (list-ref (list-ref imagen 2) 0))
-           (= (length (list-ref imagen 2)) (* (list-ref imagen 0) (list-ref imagen 1)))) #t #f))
-
-;TDA IMAGE ---- MODIFICADORES
-
-;Voltear Horizontalmente
-(define (flipH imagen)
-  (list (car imagen) (cadr imagen) (flipHpixels (list-ref imagen 2) (- (car imagen) 1))))
+; Dominio: image
+; Recorrido: image
+; Descripción: Determina si una imagen es hexmap
+; Recursión: No aplica
+(define (flipH img)
+  (list (car img) (cadr img) (flipHpixels (list-ref img 2) (- (car img) 1))))
 
 (define (flipHpixel pixel width)
   (cons (- width (car pixel)) (cdr pixel)))
@@ -62,8 +56,8 @@
       (cons (flipHpixel (car pixlist) width) (flipHpixels (cdr pixlist) width))))
 
 ;Voltear Verticalmente
-(define (flipV imagen)
-  (list (car imagen) (cadr imagen) (flipVpixels (list-ref imagen 2) (- (cadr imagen) 1))))
+(define (flipV img)
+  (list (car img) (cadr img) (flipVpixels (list-ref img 2) (- (cadr img) 1))))
 
 (define (flipVpixel pixel height)
   (append (list (car pixel) (- height (cadr pixel))) (cddr pixel)))
@@ -73,5 +67,34 @@
       null
       (cons (flipVpixel (car pixlist) height) (flipVpixels (cdr pixlist) height))))
 
+;TDA IMAGE ---- PERTENENCIA
+
+; Dominio: image
+; Recorrido: boolean
+; Descripción: Determina si una imagen está comprimida
+; Recursión: No aplica
+(define (compressed? img)
+  (if (< (length (list-ref img 2)) (* (car img) (cdar img))) #t #f))
+
+; Dominio: image
+; Recorrido: boolean
+; Descripción: Determina si una imagen es bitmap
+; Recursión: No aplica
+(define (bitmap? img)
+  (if (pixbit? (list-ref (list-ref img 2) 0)) #t #f))
+
+; Dominio: image
+; Recorrido: boolean
+; Descripción: Determina si una imagen es pixmap
+; Recursión: No aplica
+(define (pixmap? img)
+  (if (pixrgb? (list-ref (list-ref img 2) 0)) #t #f))
+
+; Dominio: image
+; Recorrido: boolean
+; Descripción: Determina si una imagen es hexmap
+; Recursión: No aplica
+(define (hexmap? img)
+  (if (pixhex? (list-ref (list-ref img 2) 0)) #t #f))
 
 (provide (all-defined-out))
